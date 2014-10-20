5e02468117d8b9bbaacce8f1fc75fc864c643348
Sean Porter <portertech@gmail.com>
2014-10-20 16:03:57 -0700

    use knife.local.rb for chef server details

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,3 +5,7 @@ knife[:cloudformation][:credentials] = {
   :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
   :region => 'us-west-2'
 }
+
+# load a ./knife.local.rb file for overrides
+knife_local_rb = File.join(File.dirname(__FILE__), 'knife.local.rb')
+Chef::Config.from_file(knife_local_rb) if File.exist?(knife_local_rb)
