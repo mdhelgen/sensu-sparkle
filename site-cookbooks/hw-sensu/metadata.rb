522b4d82a7d1c9c84e51b05c1cdf9a76af9f0971
Sean Porter <portertech@gmail.com>
2014-10-20 18:32:49 -0700

    sensu wrapper cookbook scaffolding: hw-sensu

---
+++ site-cookbooks/hw-sensu/metadata.rb
@@ -0,0 +1,9 @@
+name             'hw-sensu'
+maintainer       'Heavy Water Operations, LLC'
+maintainer_email 'support@hw-ops.com'
+license          'MIT'
+description      'Installs/Configures hw-sensu'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'sensu'
